9b1ce9ea0db93caeef386b568b81a0d837f8cd45
Omar Ghareeb <GharibCodeNinja@users.noreply.github.com>
2026-04-05 04:21:37 +0200

    feat: create model, DTOs and repository for the purchase feature (55-10412)

---
+++ src/main/java/com/example/lab05/repository/PurchaseReceiptRepository.java
@@ -0,0 +1,10 @@
+package com.example.lab05.repository;
+
+import com.example.lab05.model.mongo.PurchaseReceipt;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.List;
+
+public interface PurchaseReceiptRepository extends MongoRepository<PurchaseReceipt, String> {
+    List<PurchaseReceipt> findByPersonName(String personName);
+    List<PurchaseReceipt> findByProductCategory(String category);
+}
